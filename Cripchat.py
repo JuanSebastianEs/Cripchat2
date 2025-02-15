3fbb4f5ebac9a8127279c3218c77762d9828b394
JuanSebastianEs <graitsebas2.0@gmail.com>
2025-02-15 14:01:38 -0600

    Add files via upload

---
+++ Cripchat.py
@@ -0,0 +1,26 @@
+import requests
+import pandas as pd
+
+# Tasa actual JPY/USD con Alpha Vantage
+API_KEY = "MK5N3CCZRD0R1ROR"
+url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=JPY&to_currency=USD&apikey={API_KEY}"
+response = requests.get(url)
+data = response.json()
+tasa_actual = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+print(f"💰 1 JPY = {tasa_actual} USD")
+
+# Usando pandas_detareader (Yahoo Finance)
+
+import yfinance as yf
+
+# Descargar datos históricos de JPY/USD desde Yahoo Finance
+df = yf.download("JPY=X", start="2018-01-01", end="2023-01-01")
+
+print(df.head())  # Muestra las primeras filas del dataframe
+
+
+# Desde FRED (ekemplo: inflación en Japón)
+from pandas_datareader import data as pdr
+
+japan_cpi = pdr.get_data_fred("CPALTT01JPQ659N", start="2000-01-01")
+
